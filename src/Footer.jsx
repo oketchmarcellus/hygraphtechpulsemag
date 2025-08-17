a35f2f7f6fea67b84ebda0936efdbc8be54e959c
Your Name <you@example.com>
2025-08-17 09:45:21 +0300

    push4

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,54 +1,54 @@
 const Footer = ({  }) => {
     return (
         <>
-            <section class="footer3 cid-uTAqkzGNtI" once="footers" id="footer-3-uTAqkzGNtI">
-                <div class="container">
-                    <div class="row">
-                        <div class="row-links">
-                            <ul class="header-menu">
-                            <li class="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
-                                <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" class="text-white animate__animated animate__delay-1s animate__fadeIn">About</a>
-                            </li><li class="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
-                                <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" class="text-white animate__animated animate__delay-1s animate__fadeIn">Blog</a>
-                            </li><li class="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
-                                <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" class="text-white animate__animated animate__delay-1s animate__fadeIn">Products</a>
-                            </li><li class="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
-                                <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" class="text-white animate__animated animate__delay-1s animate__fadeIn">Contact</a>
+            <section className="footer3 cid-uTAqkzGNtI" id="footer-3-uTAqkzGNtI">
+                <div className="container">
+                    <div className="row">
+                        <div className="row-links">
+                            <ul className="header-menu">
+                            <li className="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
+                                <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" className="text-white animate__animated animate__delay-1s animate__fadeIn">About</a>
+                            </li><li className="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
+                                <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" className="text-white animate__animated animate__delay-1s animate__fadeIn">Blog</a>
+                            </li><li className="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
+                                <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" className="text-white animate__animated animate__delay-1s animate__fadeIn">Products</a>
+                            </li><li className="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
+                                <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" className="text-white animate__animated animate__delay-1s animate__fadeIn">Contact</a>
                             </li></ul>
                         </div>
 
-                        <div class="col-12 mt-4">
-                            <div class="social-row">
-                                <div class="soc-item">
-                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-                                        <span class="mbr-iconfont socicon socicon-facebook display-7"></span>
+                        <div className="col-12 mt-4">
+                            <div className="social-row">
+                                <div className="soc-item">
+                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" className="animate__animated animate__delay-1s animate__fadeIn">
+                                        <span className="mbr-iconfont socicon socicon-facebook display-7"></span>
                                     </a>
                                 </div>
-                                <div class="soc-item">
-                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-                                        <span class="mbr-iconfont socicon-twitter socicon"></span>
+                                <div className="soc-item">
+                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" className="animate__animated animate__delay-1s animate__fadeIn">
+                                        <span className="mbr-iconfont socicon-twitter socicon"></span>
                                     </a>
                                 </div>
-                                <div class="soc-item">
-                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-                                        <span class="mbr-iconfont socicon-instagram socicon"></span>
+                                <div className="soc-item">
+                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" className="animate__animated animate__delay-1s animate__fadeIn">
+                                        <span className="mbr-iconfont socicon-instagram socicon"></span>
                                     </a>
                                 </div>
-                                <div class="soc-item">
-                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-                                        <span class="mbr-iconfont socicon socicon-linkedin"></span>
+                                <div className="soc-item">
+                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" className="animate__animated animate__delay-1s animate__fadeIn">
+                                        <span className="mbr-iconfont socicon socicon-linkedin"></span>
                                     </a>
                                 </div>
-                                <div class="soc-item">
-                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-                                        <span class="mbr-iconfont socicon socicon-twitch"></span>
+                                <div className="soc-item">
+                                    <a href="https://ai-builder.mobirise.com/pages/iUc7ROw3Kwx452_MyEyid#" target="_blank" className="animate__animated animate__delay-1s animate__fadeIn">
+                                        <span className="mbr-iconfont socicon socicon-twitch"></span>
                                     </a>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="col-12 mt-5">
-                            <p class="mbr-fonts-style copyright display-7 animate__animated animate__delay-1s animate__fadeIn">
+                        <div className="col-12 mt-5">
+                            <p className="mbr-fonts-style copyright display-7 animate__animated animate__delay-1s animate__fadeIn">
                                 © 2025 TechInnovate. All rights reserved.
                             </p>
                         </div>
